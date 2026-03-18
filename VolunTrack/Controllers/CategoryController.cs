3527afa078467b971204f1fbe82c7c7d3640e590
UnknownFruit <kurianovip@gmail.com>
2026-03-18 22:23:30 +0400

    feat(api): add GET /api/categories endpoint

---
+++ VolunTrack/Controllers/CategoryController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VolunTrack.DTO;
+using VolunTrack.Repositories;
+
+namespace VolunTrack.Controllers
+{
+    [Route("api/categories")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoryController(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var categories = await _categoryRepository.GetAllActiveAsync();
+            return Ok(categories.Select(c => new CategoryDto
+            {
+                Id = c.Id,
+                Name = c.Name,
+                Description = c.Description,
+                ColorRgb = c.ColorRgb
+            }));
+        }
+    }
+}
